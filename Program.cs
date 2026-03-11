83023bb0895bd23d4e9774bcd2893a17d648eb4a
Ivan Ramanchuk <s33296@pjwstk.edu.pl>
2026-03-11 13:49:58 +0100

    fun CalculateAverage

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Enter integers:");
 int? input1 = int.Parse(Console.ReadLine());
 int? input2 = int.Parse(Console.ReadLine());
+int[] numbers = { 1, 2, 3, 4, 5 };
 
 if (input1 == null && input2 == null)
 {
@@ -12,3 +13,9 @@ Console.WriteLine($"Average: {(input1 + input2)/2}");
 Console.WriteLine($"Sum: {input1 + input2}: ");
 Console.WriteLine($"Subtraction: {input1 - input2}: ");
 Console.WriteLine($"Multiplication: {input1 * input2}: ");
+Console.WriteLine(CalculateAverage(numbers));
+
+int CalculateAverage(int[] values)
+{
+    return values.Sum() / values.Length;
+}
