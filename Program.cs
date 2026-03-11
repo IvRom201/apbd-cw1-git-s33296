75be5946241d3d8f15ed736153073bf9c1282239
Ivan Ramanchuk <s33296@pjwstk.edu.pl>
2026-03-11 13:12:34 +0100

    Premitive calculator

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter integers:");
+int? input1 = int.Parse(Console.ReadLine());
+int? input2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Average: {(input1 + input2)/2}");
+Console.WriteLine($"Sum: {input1 + input2}: ");
+Console.WriteLine($"Subtraction: {input1 - input2}: ");
