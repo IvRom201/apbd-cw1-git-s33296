1d5201358f0823c7063377d7747c322eddc37cb5
Ivan Ramanchuk <s33296@pjwstk.edu.pl>
2026-03-11 13:15:31 +0100

    Add null validation

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 int? input1 = int.Parse(Console.ReadLine());
 int? input2 = int.Parse(Console.ReadLine());
 
+if (input1 == null && input2 == null)
+{
+    Console.WriteLine("Please enter an integers");
+    return;
+}
+
 Console.WriteLine($"Average: {(input1 + input2)/2}");
 Console.WriteLine($"Sum: {input1 + input2}: ");
 Console.WriteLine($"Subtraction: {input1 - input2}: ");
